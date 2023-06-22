e661773035181fc42b9ed485c4371c9c82a5e2ca
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2023-06-22 10:43:00 +0530

    Articles and Inventory App createdgit add .

--- Inventory_management_project/views.py
+++ Inventory_management_project/views.py
@@ -1,2 +1,12 @@
 """Django views are Python functions that takes http requests and returns http response, like HTML documents.
 """
+from django.http import HttpResponse
+
+
+def home(request):
+    """Take an request and shwo it on web page.
+
+    Args:
+        request (_type_): Django sends a request to render data on web
+    """
+    return HttpResponse("<center><h1>This is From Django Web FrameWork</h1></center>")
